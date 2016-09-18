aff366d4c8997636419d173ffd35aeaf7f402853
Saeed Ganji( Shahab ) <shahabganji@gmail.com>
2016-09-18 22:42:48 +0430

    Starting scrollable multiple choice

---
+++ src/InteractiveConsole/Implementation/ScrollableMultipleChoice.cs
@@ -0,0 +1,13 @@
+namespace LifeResource.Terminal
+{
+
+    public class ScrollabelMultipleChoice : MultipleChoice
+    {
+
+        public ScrollabelMultipleChoice(string question) : base(question)
+        {
+
+        }
+    }
+
+}
