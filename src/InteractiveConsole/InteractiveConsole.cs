a4262f39495a1ab06d98489d2719969ed6fe4563
Saeed Ganji( Shahab ) <shahabganji@gmail.com>
2016-09-18 21:08:59 +0430

    namespace changed to terminal

--- src/InteractiveConsole/InteractiveConsole.cs
+++ src/InteractiveConsole/InteractiveConsole.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace LifeResource
+namespace LifeResource.Terminal
 {
 
     public class InteractiveConsole
--- src/InteractiveConsole/Iteration.cs
+++ src/InteractiveConsole/Iteration.cs
@@ -1,6 +1,6 @@
 using System.Collections.Generic;
 
-namespace LifeResource
+namespace LifeResource.Terminal
 {
 
     public class Iteration
